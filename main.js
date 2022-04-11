e73489dd247707bed6940d02c1dad5f48111b9fe
vitrag1999 <95957595+vitrag1999@users.noreply.github.com>
2022-04-11 17:25:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+incrememt = 1;
+ score = 1;
+
+function drawBall() {
+    
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    document.getElementById("button").addEventListener('click', start);
+    function start(){
+    score = score + 1
+    document.getElementById("score").innerHTML = 0;
+
+    // console.log(score);if it doesnot show in the ux just do it console.log it will show 
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
